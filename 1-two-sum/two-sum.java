181522a1e2ccd3e67b7d2709bb3ece95421ad142
jatinrathod1 <134234094+jatinrathod1@users.noreply.github.com>
2024-02-09 23:18:14 +0530

    Time: 3 ms (70.91%) | Memory: 44.8 MB (42.98%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,41 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-         int arr[]=new int[2];
-        for(int i=0; i<nums.length ; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(nums[i]+nums[j]==target){
-                    arr[0]=i;
-                    arr[1]=j;
-                    return arr;}
-            }
+import java.util.Arrays;
 
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] originalIndices = new int[2];
+        int[] sortedNums = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(sortedNums);
+
+        int left = 0;
+        int right = sortedNums.length - 1;
+
+        while (left < right) {
+            int sum = sortedNums[left] + sortedNums[right];
+
+            if (sum == target) {
+                // Find the original indices in the unsorted array
+                originalIndices[0] = findIndex(nums, sortedNums[left]);
+                originalIndices[1] = findIndex(nums, sortedNums[right]);
+                return originalIndices;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
         }
-        arr[0]=-1;
-        arr[1]=-1;
-        return arr;
+
+        // If no solution is found
+        return new int[]{-1, -1};
+    }
+
+    private int findIndex(int[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                // Mark the found element as Integer.MIN_VALUE to avoid reusing it
+                array[i] = Integer.MIN_VALUE;
+                return i;
+            }
+        }
+        return -1;
     }
 }
