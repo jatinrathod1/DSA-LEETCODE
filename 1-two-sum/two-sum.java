e3da056248176802bf229a54ecff4c971e751575
jatinrathod1 <134234094+jatinrathod1@users.noreply.github.com>
2024-02-10 00:01:04 +0530

    Time: 2 ms (95.62%) | Memory: 44.9 MB (30.27%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,41 +1,16 @@
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] originalIndices = new int[2];
-        int[] sortedNums = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(sortedNums);
+          Map<Integer, Integer> map = new HashMap<>();
 
-        int left = 0;
-        int right = sortedNums.length - 1;
-
-        while (left < right) {
-            int sum = sortedNums[left] + sortedNums[right];
-
-            if (sum == target) {
-                // Find the original indices in the unsorted array
-                originalIndices[0] = findIndex(nums, sortedNums[left]);
-                originalIndices[1] = findIndex(nums, sortedNums[right]);
-                return originalIndices;
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
+        for (int j = 0; j < nums.length; j++) {
+            int complete = target - nums[j];
+            if(map.containsKey(complete))
+            {
+                return new int[]{j , map.get(complete)};
             }
-        }
-
-        // If no solution is found
-        return new int[]{-1, -1};
-    }
-
-    private int findIndex(int[] array, int value) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == value) {
-                // Mark the found element as Integer.MIN_VALUE to avoid reusing it
-                array[i] = Integer.MIN_VALUE;
-                return i;
-            }
-        }
-        return -1;
+            map.put(nums[j], j);
+        }    
+        throw new IllegalArgumentException("error");
+    
     }
 }
