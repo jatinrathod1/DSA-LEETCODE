1c7f6a1976398187367ee7cccdc6f042f91040cf
jatinrathod1 <134234094+jatinrathod1@users.noreply.github.com>
2024-08-18 16:03:37 +0530

    Time: 10 ms (57.33%) | Memory: 45.3 MB (11.05%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean canConstruct(String r, String m) {
-    HashMap<Character, Integer> map = new HashMap<>();
-    //map ma value nakhi
-    for (char c: m.toCharArray()){
-        map.put(c,map.getOrDefault(c,0)+1);
-    }
-    // have r no darek char m na map che ke nai te check karie
-        for (int i=0; i<r.length(); i++){
-            char c= r.charAt(i);
-            if(!map.containsKey(c) || map.get(c)==0){
+        HashMap<Character, Integer> mp = new HashMap<>();
+        // map ma value nakhavani
+        for (char c : m.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        }
+        //have r na badha char m ma check karavani
+        for(int i=0; i<r.length(); i++){
+            char c = r.charAt(i);
+            if(!mp.containsKey(c) || mp.get(c) == 0 ){
                 return false;
             }
-            map.put(c,map.get(c)-1);
-        }
+            mp.put(c,mp.get(c)-1);
+        } 
         return true;
     }
 }
