c003e3457db535b5c96ee7a0b2f8b98e344bed11
jatinrathod1 <134234094+jatinrathod1@users.noreply.github.com>
2024-08-24 00:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (11.91%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.java
+++ 898-transpose-matrix/transpose-matrix.java
@@ -4,8 +4,8 @@ class Solution {
         int n = matrix[0].length;
         int[][] tr = new int[n][m];
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < m; i++) {   // raw
+            for (int j = 0; j < n; j++) { // raw ni coloums
                 tr[j][i] = matrix[i][j];
             }
         }
