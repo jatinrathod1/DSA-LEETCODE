397dc4a45d683eb23aa8d60829aab6a1f037a9fc
jatinrathod1 <134234094+jatinrathod1@users.noreply.github.com>
2024-08-31 15:18:12 +0530

    Time: 2 ms (52.27%) | Memory: 41.4 MB (52.79%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char poped = stack.pop();
+                if ((s.charAt(i) == '}' && poped != '{') || (s.charAt(i) == ')' && poped != '(')
+                        || (s.charAt(i) == ']' && poped != '[')) {
+                    return false;
+
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
