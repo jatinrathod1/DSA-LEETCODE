c0994d3ef787337b24bdad39dc84998a1e4cbad2
jatinrathod1 <134234094+jatinrathod1@users.noreply.github.com>
2024-09-01 13:34:54 +0530

    Time: 2 ms (52.26%) | Memory: 41.3 MB (70.66%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -4,8 +4,8 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '{' || s.charAt(i) == '(' || s.charAt(i) == '[') {
                 stack.push(s.charAt(i));
-
-            } else {
+            } 
+            else {
                 if (stack.isEmpty()) {
                     return false;
                 }
