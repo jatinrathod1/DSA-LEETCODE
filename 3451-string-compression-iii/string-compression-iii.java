99f675c1c0a6e7e0bf242592b69e8dd094182c07
Jatin Rathod <134234094+jatinrathod1@users.noreply.github.com>
2024-11-04 16:03:02 +0530

    Time: 1947 ms (5.08%) | Memory: 51.7 MB (8.75%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String compressedString(String word) {
+        int n =word.length();
+        String an="";
+        for(int i=0; i<n; i++){
+            char ch = word.charAt(i);
+            int c=1;
+            i++;
+            while((i<n) && ch == word.charAt(i) ){
+                c++;
+                i++;
+            }
+            while(c>9){
+                an+='9';
+                an+=ch;
+                c=c-9;
+            }
+            
+            an+=c;
+            an+=ch;
+            i--;
+        }
+    return an;
+    }
+}
